2f87e06716eedc70023cc21d7e7add9a95bd21cc
Avi <avirzajac@gmail.com>
2017-08-08 15:35:06 -0400

    Center the header and app info

--- client/src/components/app-info.js
+++ client/src/components/app-info.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './app-info.css';
 
 export default function AppInfo() {
     //On the page rendering the register/sign in page this will display information about the app for the user to know what the app is about!
-    return <p>Dreaming of Denmark? Join us with learning Danish (Dansk) by reviewing words with us and test your knowledge out after!</p>;
+    return <div className="app-info-p"><p>Dreaming of Denmark? Join us with learning Danish (Dansk) by reviewing words with us and test your knowledge out after!</p></div>;
 }

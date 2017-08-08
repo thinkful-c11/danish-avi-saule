946291ab526d5d00e2896ce8119d3ad68afe14c6
Avi <avirzajac@gmail.com>
2017-08-08 15:24:28 -0400

    Styling just so it's not massively big on the page for now

--- client/src/components/google-sign-in.js
+++ client/src/components/google-sign-in.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './google-sign-in.css';
 
 export default function GoogleSignIn() {
-    return <div className="google-button"><a href={'/api/auth/google'}><img src="../../images/google-logo.png" alt="Google G logo"/>Register/Sign in</a></div>;
+    return <div className="google-button"><a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G logo"/>Register/Sign in</a></div>;
 }
--- client/src/components/sign-out.js
+++ client/src/components/sign-out.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './sign-out.css';
 
 export default function SignOut() {
-    return <a href={'/api/auth/google'}><img src="../../images/google-logo.png" alt="Google G Logo" />Sign Out</a>;
+    return <div className="google-button"><a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G Logo" />Sign Out</a></div>;
 }

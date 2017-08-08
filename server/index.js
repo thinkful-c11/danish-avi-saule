bc875aef5d878c352b941f74bced81846876fe59
Saule BN <saulebohoney@gmail.com>
2017-08-08 10:25:12 -0400

    switching repos

--- server/index.js
+++ server/index.js
@@ -17,19 +17,12 @@ mongoose.connect(DATABASE_URL,function(err){
     console.log('MLab connected!');
 });
 
-let secret = {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
-}
 
 if(process.env.NODE_ENV != 'production') {
-    secret = require('secret');
+    secret = require('./secret');
 }
 
 const app = express();
-
-// const database = {
-// };
 
 app.use(passport.initialize());
 app.use(bodyParser.json());
@@ -91,66 +84,65 @@ passport.use(
         // google id, and the access token
         // Job 2: Update this callback to either update or create the user
         // so it contains the correct access token
-    
-        return cb(null, user);
-    }
-));
+
 
 passport.use(
     new BearerStrategy(
         (token, done) => {
+            User.find({accessToken: token}, function(err, user){
+                if(!user.length) {
+                    return done(null, false);
+                }
             // Job 3: Update this callback to try to find a user with a
             // matching access token.  If they exist, let em in, if not,
             // don't.
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
+                return done(null, user);
+            });
         }
     )
 );
 
-// app.get('/api/auth/google',
-//     passport.authenticate('google', {scope: ['profile']}));
+app.get('/api/auth/google',
+    passport.authenticate('google', {scope: ['profile']}));
 
-// app.get('/api/auth/google/callback',
-//     passport.authenticate('google', {
-//         failureRedirect: '/',
-//         session: false
-//     }),
-//     (req, res) => {
-//         res.cookie('accessToken', req.user.accessToken, {expires: 0});
-//         res.redirect('/');
-//     }
-// );
+app.get('/api/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/',
+        session: false
+    }),
+    (req, res) => {
+        res.cookie('accessToken', req.user.accessToken, {expires: 0});
+        res.redirect('/');
+    }
+);
 
-// app.get('/api/auth/logout', (req, res) => {
-//     req.logout();
-//     res.clearCookie('accessToken');
-//     res.redirect('/');
-// });
+app.get('/api/auth/logout', (req, res) => {
+    req.logout();
+    res.clearCookie('accessToken');
+    res.redirect('/');
+});
 
-// app.get('/api/me',
-//     passport.authenticate('bearer', {session: false}),
-//     (req, res) => res.json({
-//         googleId: req.user.googleId
-//     })
-// );
+app.get('/api/me',
+    passport.authenticate('bearer', {session: false}),
+    (req, res) => res.json({
+        googleId: req.user.googleId
+    })
+);
 
-// app.get('/api/questions',
-//     passport.authenticate('bearer', {session: false}),
-//     (req, res) => res.json(['Question 1', 'Question 2'])
-// );
+app.get('/api/questions',
+    passport.authenticate('bearer', {session: false}),
+    (req, res) => res.json(['Question 1', 'Question 2'])
+);
 
-// // Serve the built client
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// Serve the built client
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// // Unhandled requests which aren't for the API should serve index.html so
-// // client-side routing using browserHistory can function
-// app.get(/^(?!\/api(\/|$))/, (req, res) => {
-//     const index = path.resolve(__dirname, '../client/build', 'index.html');
-//     res.sendFile(index);
-// });
+// Unhandled requests which aren't for the API should serve index.html so
+// client-side routing using browserHistory can function
+app.get(/^(?!\/api(\/|$))/, (req, res) => {
+    const index = path.resolve(__dirname, '../client/build', 'index.html');
+    res.sendFile(index);
+});
 
 let server;
 function runServer(port=3001) {

062b8223aa5df14d5e3a03a0b1964d93512f1f2c
Avi <avirzajac@gmail.com>
2017-08-09 15:49:22 -0400

    Changed button to just a href for now

--- client/src/components/google-sign-in.js
+++ client/src/components/google-sign-in.js
@@ -4,8 +4,8 @@ import './google-sign-in.css';
 export default function GoogleSignIn() {
     return (
         <div className="google-button">
-            <button className="button-google">
+            {/* <button className="button-google"> */}
                 <a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G logo"/>Register/Sign in</a>
-            </button>
+            {/* </button> */}
         </div>);
 }

bcbc3cb47f13c6c5577b0aea0849631d9eb1aa6f
Avi <avirzajac@gmail.com>
2017-08-09 11:15:37 -0400

    Updated sign in and sign out buttons

--- client/src/components/google-sign-in.js
+++ client/src/components/google-sign-in.js
@@ -2,5 +2,10 @@ import React from 'react';
 import './google-sign-in.css';
 
 export default function GoogleSignIn() {
-    return <div className="google-button"><a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G logo"/>Register/Sign in</a></div>;
+    return (
+        <div className="google-button">
+            <button className="button-google">
+                <a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G logo"/>Register/Sign in</a>
+            </button>
+        </div>);
 }
--- client/src/components/sign-out.js
+++ client/src/components/sign-out.js
@@ -2,5 +2,10 @@ import React from 'react';
 import './sign-out.css';
 
 export default function SignOut() {
-    return <div className="google-button"><a className="fill-google-button" href={'/api/auth/google'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G Logo" />Sign Out</a></div>;
+    return (
+        <div className="google-button">
+            <button className="button-google">
+                <a className="fill-google-button" href={'/api/auth/logout'}><img className="google-logo" src="../../images/google-logo.png" alt="Google G Logo" />Sign Out</a>
+            </button>
+        </div>);
 }

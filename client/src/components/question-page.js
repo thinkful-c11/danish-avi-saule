0cf3286402b4b6bb242d0ce1d9f8422a6a0f9581
Avi <avirzajac@gmail.com>
2017-08-10 14:38:59 -0400

    Figured out index needed to get single value for questions

--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -32,12 +32,23 @@ export default class QuestionPage extends React.Component {
         const questions = this.state.questions.map((question, index) =>
             <li key={index}>{question}</li>
         );
+        // console.log("This is questions:",{questions});
+        // console.log("This is state",this.state);
+        // console.log("This is state.questions",this.state.questions);
+        // console.log("This is state.questions[0]",this.state.questions[0]);
+        // var result = objArray.map(function(a) {return a.foo;});
+        const questionsAnswers=this.state.questions;
+        console.log("This is questionsAnswers",questionsAnswers);
+        let englishTranslation=questionsAnswers.map(function(questionValue){return questionValue.question});
+        console.log("This is englishTranslation",englishTranslation);
+        console.log("This is the first index?",englishTranslation[0]);
 
         return (
             // <ul className="question-list">
             //     {questions}
             // </ul>
             <div className='questions-page'>
+                <div class='english-question-here'>{englishTranslation[0]}</div>
                 <form>
                     {questions}
                     <input type='text' placeholder='Type in the English translation'></input>

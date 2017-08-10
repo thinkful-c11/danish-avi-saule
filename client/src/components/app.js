e49d2002115b9fa29bff0699b66f0f7ee3fc25e6
Saule BN <saulebohoney@gmail.com>
2017-08-10 12:03:54 -0400

    message

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -14,6 +14,7 @@ class App extends React.Component {
         // Job 4: Redux-ify all of the state and fetch calls to async actions.
         //DONE?
         const accessToken = Cookies.get('accessToken');
+        console.log(accessToken);
         if (accessToken) {
             this.props.dispatch(getUser(accessToken));
         }
